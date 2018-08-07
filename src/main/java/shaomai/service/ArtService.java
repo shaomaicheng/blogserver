5d603c8aeece2fea62f113c7fa65b0cfd788ff04
shaomaicheng <devchenglei@gmail.com>
2018-08-07 08:45:58 +0800

    init server's repository

---
+++ src/main/java/shaomai/service/ArtService.java
@@ -0,0 +1,12 @@
+package shaomai.service;
+
+import shaomai.exception.ArtSelectException;
+import shaomai.model.p.ArtBean;
+
+import java.util.List;
+
+public interface ArtService {
+    List<ArtBean> getAllArts(int pageNo,int pageSize) throws ArtSelectException;
+    int artTotal () throws ArtSelectException;
+    ArtBean getArtById(int id) throws ArtSelectException;
+}
